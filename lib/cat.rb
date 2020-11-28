4310b43dd4b914e0473272b62f81b431da33b0cd
Charles <charlesangeloplai@gmail.com>
2020-11-27 23:23:41 -0500

    commit tree cb173357a91486383fc31689521c802070ef7118

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(pet_name, owner_name)
+    @name = pet_name
+    @owner = owner_name
+    @mood = "nervous"
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
